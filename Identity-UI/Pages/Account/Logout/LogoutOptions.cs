72bf80ded88ab54fca7bafd7ed049d2bf8809521
gfarah7 <gfarah7@gatech.edu>
2022-08-24 23:24:43 -0400

    clean-up IdentityServer Razer UI Pages

--- Identity-UI/Pages/Account/Logout/LogoutOptions.cs
+++
@@ -1,8 +0,0 @@
-
-namespace Identity_UI.Pages.Logout;
-
-public class LogoutOptions
-{
-    public static bool ShowLogoutPrompt = true;
-    public static bool AutomaticRedirectAfterSignOut = false;
-}
